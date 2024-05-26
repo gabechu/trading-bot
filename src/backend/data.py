4b2ba78126ce71cc2f69f51ac6014a639ec81373
gchu <gchu@zendesk.com>
2024-05-26 06:20:57 +0000

    cleanup remove unused code

--- src/backend/data.py
+++ src/backend/data.py
@@ -26,16 +26,3 @@ def read_data(ticker: str, start: str, end: str, multiplier: int, timespan: Time
         f"https://api.polygon.io/v2/aggs/ticker/{ticker}/range/{multiplier}/{timespan.value}/{start}/{end}?adjusted=true&sort=asc&apiKey={POLYGON_API_KEY}"
     ).json()
     return pd.DataFrame(response["results"])
-
-
-# start = "2021-05-01"
-# end = "2022-05-01"
-# multiplier = "1"
-# timespan = "minute"
-
-# response = requests.get(
-#     f"https://api.polygon.io/v2/aggs/ticker/{ticker}/range/{multiplier}/{timespan}/{start}/{end}?adjusted=true&sort=asc&apiKey={POLYGON_API_KEY}"
-# ).json()
-# data = pd.DataFrame(response["results"])
-
-# save_path = f"data/{ticker}/{multiplier}_{timespan}/from_{start}_to_{end}.csv"
